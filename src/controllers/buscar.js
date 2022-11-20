6a680204e32b66653c5c299dfad8b7bd65c93293
jairoalperez <jairoalberto98@hotmail.com>
2022-11-20 17:41:48 -0500

    Update buscar.js

--- src/controllers/buscar.js
+++ src/controllers/buscar.js
@@ -1,10 +1,6 @@
 const buscar = {}
 const bd = require('../database')
 
-post.crear= (req,res)=>{
-    try{bd.crearpost(req,res);}catch(e){console.log(e); }    
-}
-
 buscar.bu = (req,res) => {
     try {bd.buser(req,res);
     } catch (e) {
